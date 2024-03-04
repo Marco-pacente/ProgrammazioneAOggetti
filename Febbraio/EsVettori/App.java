da9a033ddc9c3e6310c761d360ec821463a07a1b
Marco-pacente <marco.pacente@gmail.com>
2024-03-04 14:57:32 +0100

    Esercizio vettori

---
+++ Febbraio/EsVettori/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        Vettore v = new Vettore(10);
+        v.stampa();
+        System.out.println("Somma: " + v.somma(10));
+    }
+}
