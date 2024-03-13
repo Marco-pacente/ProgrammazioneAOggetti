61a3411f66a7c9ccba8dc2b5f7948bcbf6fba5bd
Marco-pacente <marco.pacente@gmail.com>
2024-03-13 12:13:27 +0100

    aggiunto log su ricorsione

--- Ricorsione/Ricorsione.java
+++ Ricorsione/Ricorsione.java
@@ -3,10 +3,12 @@ public class Ricorsione {
         if (n == 0 || n == -1) {
             return 1;
         } else {
-            return n * fattoriale(n - 1);
+            double f = fattoriale(n-1);
+            //System.out.println(f);
+            return n * f;
         }
     }
     public static void main(String[] args) {
-        System.out.println("Fattoriale 10: " + fattoriale(500000));
+        System.out.println("Fattoriale 500: " + fattoriale(500));
     }
 }
